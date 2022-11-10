831dcf3d907eae321eb5a4f6885a96d5b2e7831a
slinky <49608195+slinkstr@users.noreply.github.com>
2022-11-09 18:21:55 -0800

    Refactor : Optimize log reading.

--- src/Util/LogReadUtil.cs
+++ src/Util/LogReadUtil.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
@@ -38,6 +39,7 @@ namespace FortniteOverlay.Util
                             Program.form.Log("Fortnite restarted, resetting log file.");
                             fs.Seek(0, SeekOrigin.Begin);
                             sr.DiscardBufferedData();
+                            Program.fortniters.Clear();
                         }
                         totalLenCached = fs.Length;
                         Thread.Sleep(1000);
@@ -48,7 +50,7 @@ namespace FortniteOverlay.Util
 
         public static void ProcessLine(string line)
         {
-            var match = Regex.Match(line, FortniteLogRegex.LoggedIn);
+            var match = FortniteLogRegex.LoggedIn.Match(line);
             if (match.Success)
             {
                 Program.hostId = match.Groups[1].ToString();
@@ -59,7 +61,7 @@ namespace FortniteOverlay.Util
                 return;
             }
 
-            match = Regex.Match(line, FortniteLogRegex.PartyMemberJoined);
+            match = FortniteLogRegex.PartyMemberJoined.Match(line);
             if (match.Success)
             {
                 if (Program.fortniters.Any(x => x.Name == match.Groups[1].ToString())) { return; }
@@ -73,7 +75,7 @@ namespace FortniteOverlay.Util
                 return;
             }
 
-            match = Regex.Match(line, FortniteLogRegex.PartyMemberLeft);
+            match = FortniteLogRegex.PartyMemberLeft.Match(line);
             if (match.Success)
             {
                 var leaver = Program.fortniters.Find(x => x.Name == match.Groups[1].ToString());
@@ -83,14 +85,14 @@ namespace FortniteOverlay.Util
                 return;
             }
 
-            match = Regex.Match(line, FortniteLogRegex.StartedGame);
+            match = FortniteLogRegex.StartedGame.Match(line);
             if (match.Success)
             {
                 Program.form.Log("[StartedGame]");
                 Program.inGame = true;
                 return;
             }
-            match = Regex.Match(line, FortniteLogRegex.LeftGame);
+            match = FortniteLogRegex.LeftGame.Match(line);
             if (match.Success)
             {
                 Program.form.Log("[LeftGame]");
@@ -100,15 +102,17 @@ namespace FortniteOverlay.Util
         }
     }
 
-    public static class FortniteLogRegex
+    //language=regex
+    public static class FortniteLogRegex 
     {
-        private static string Timestamp = @"\[\d{4}\.\d{2}\.\d{2}\-\d{2}\.\d{2}\.\d{2}:\d{3}]";
-        private static string UnknownId = @"\[[\d ]{3}\]";
-        public static string LoggedIn = "^" + Timestamp + UnknownId + @"LogOnlineAccount: Display: \[UOnlineAccountCommon::ProcessUserLogin\] Successfully logged in user\. UserId=\[([0-9a-fA-F]{32})\] DisplayName=\[(.{1,32})\] EpicAccountId=\[MCP:[0-9a-fA-F]{32}\] AuthTicket=\[<Redacted>\]$";
-        public static string PartyMemberJoined = "^" + Timestamp + UnknownId + @"LogParty: Display: New party member state for \[(.{1,32})\] Id \[MCP:([0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5})\] added to the local player's party \[V2:[0-9a-fA-F]{32}\]\.$";
-        public static string PartyMemberLeft = "^" + Timestamp + UnknownId + @"LogParty: Display: Party member state for \[(.{1,32})\] Id \[MCP:([0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5})\] removed from \[(.{1,32})\]'s party\.$";
-        public static string StartedGame = "^" + Timestamp + UnknownId + @"LogDemo: UReplaySubsystem::RecordReplay: Starting recording with demo driver\.  Name:  FriendlyName: Unsaved Replay$";
-        //public static string StartedGameAlt = "^" + Timestamp + UnknownId + @"LogLocalFileReplay: Writing replay to '.*' with \d+\.\d{2}MB free$";
-        public static string LeftGame = "^" + Timestamp + UnknownId + @"LogDemo: StopDemo: Demo  stopped at frame \d+$";
+        private static readonly string TimestampComp       = @"\[\d{4}\.\d{2}\.\d{2}\-\d{2}\.\d{2}\.\d{2}:\d{3}]";
+        private static readonly string UnknownIdComp       = @"\[[\d ]{3}\]";
+        private static readonly string LineStartComp       = "^" + TimestampComp + UnknownIdComp;
+        public static readonly Regex LoggedIn          = new Regex(LineStartComp + @"LogOnlineAccount: Display: \[UOnlineAccountCommon::ProcessUserLogin\] Successfully logged in user\. UserId=\[([0-9a-fA-F]{32})\] DisplayName=\[(.{1,32})\] EpicAccountId=\[MCP:[0-9a-fA-F]{32}\] AuthTicket=\[<Redacted>\]$", RegexOptions.Compiled | RegexOptions.Singleline);
+        public static readonly Regex PartyMemberJoined = new Regex(LineStartComp + @"LogParty: Display: New party member state for \[(.{1,32})\] Id \[MCP:([0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5})\] added to the local player's party \[V2:[0-9a-fA-F]{32}\]\.$",                                        RegexOptions.Compiled | RegexOptions.Singleline);
+        public static readonly Regex PartyMemberLeft   = new Regex(LineStartComp + @"LogParty: Display: Party member state for \[(.{1,32})\] Id \[MCP:([0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5})\] removed from \[(.{1,32})\]'s party\.$",                                                                  RegexOptions.Compiled | RegexOptions.Singleline);
+        public static readonly Regex StartedGame       = new Regex(LineStartComp + @"LogDemo: UReplaySubsystem::RecordReplay: Starting recording with demo driver\.  Name:  FriendlyName: Unsaved Replay$",                                                                                                        RegexOptions.Compiled | RegexOptions.Singleline);
+        public static readonly Regex StartedGameAlt    = new Regex(LineStartComp + @"LogLocalFileReplay: Writing replay to '.*' with \d+\.\d{2}MB free$",                                                                                                                                                          RegexOptions.Compiled | RegexOptions.Singleline);
+        public static readonly Regex LeftGame          = new Regex(LineStartComp + @"LogDemo: StopDemo: Demo  stopped at frame \d+$",                                                                                                                                                                              RegexOptions.Compiled | RegexOptions.Singleline);
     }
 }
