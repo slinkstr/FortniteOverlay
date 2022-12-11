d24810e6ab38553aa0a58a353c88f8e2abe5dbee
slinky <49608195+slinkstr@users.noreply.github.com>
2022-12-11 02:51:43 -0800

    Add : Party index back, might be useful and doesn't hurt if it's not.

--- src/Util/LogReader.cs
+++ src/Util/LogReader.cs
@@ -61,20 +61,63 @@ namespace FortniteOverlay.Util
                 return;
             }
 
-            match = FortniteLogRegex.PartyMemberJoined.Match(line);
+            //match = FortniteLogRegex.PartyMemberJoined.Match(line);
+            //if (match.Success)
+            //{
+            //    string name = match.Groups["DisplayName"].ToString();
+            //
+            //    if (Program.hostName == name)                    { return; }
+            //    if (Program.fortniters.Any(x => x.Name == name)) { return; }
+            //
+            //    Fortniter newJoin = new Fortniter()
+            //    {
+            //        Name = name,
+            //    };
+            //    Program.fortniters.Add(newJoin);
+            //    Program.form.Log("[PartyMemberJoined] Name: " + name);
+            //    return;
+            //}
+
+            match = FortniteLogRegex.PartyMemberJoinedAlt.Match(line);
             if (match.Success)
             {
                 string name = match.Groups["DisplayName"].ToString();
+                int index = Int32.Parse(match.Groups["PartyIndex"].ToString());
 
                 if (Program.hostName == name)                    { return; }
-                if (Program.fortniters.Any(x => x.Name == name)) { return; }
+
+                var matchingPlayer = Program.fortniters.FirstOrDefault(x => x.Name == name);
+                if (matchingPlayer != null)
+                {
+                    matchingPlayer.PartyIndex = index;
+                    return;
+                }
 
                 Fortniter newJoin = new Fortniter()
                 {
                     Name = name,
+                    PartyIndex = index,
                 };
+
                 Program.fortniters.Add(newJoin);
-                Program.form.Log("[PartyMemberJoined] Name: " + name);
+                Program.form.Log("[PartyMemberJoinedAlt] Name: " + name + ", index: " + index);
+                return;
+            }
+
+            match = FortniteLogRegex.PartyMemberUpdated.Match(line);
+            if (match.Success)
+            {
+                string name = match.Groups["DisplayName"].ToString();
+                int index = Int32.Parse(match.Groups["PartyIndex"].ToString());
+
+                if (Program.hostName == name) { return; }
+
+                var matchingPlayer = Program.fortniters.FirstOrDefault(x => x.Name == name);
+                if (matchingPlayer != null)
+                {
+                    matchingPlayer.PartyIndex = index;
+                }
+                //Program.form.Log("[PartyMemberUpdated] Name: " + name + ", index: " + index);
                 return;
             }
 
@@ -108,16 +151,17 @@ namespace FortniteOverlay.Util
         //language=regex
         private static class FortniteLogRegex
         {
-            private static readonly string TimestampComp = @"\[\d{4}\.\d{2}\.\d{2}\-\d{2}\.\d{2}\.\d{2}:\d{3}]";
-            private static readonly string UnknownIdComp = @"\[[\d ]{3}\]";
-            private static readonly string LineStartComp = "^" + TimestampComp + UnknownIdComp;
-            public static readonly Regex LoggedIn = new Regex(LineStartComp + @"LogOnlineAccount: Display: \[UOnlineAccountCommon::ProcessUserLogin\] Successfully logged in user\. UserId=\[[0-9a-fA-F]{32}\] DisplayName=\[(?<DisplayName>.{1,16})\] EpicAccountId=\[MCP:[0-9a-fA-F]{32}\] AuthTicket=\[<Redacted>\]$", RegexOptions.Compiled | RegexOptions.Singleline);
-            public static readonly Regex PartyMemberJoined = new Regex(LineStartComp + @"LogParty: Display: New party member state for \[(?<DisplayName>.{1,16})\] Id \[MCP:[0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5}\] added to the local player's party \[V2:[0-9a-fA-F]{32}\]\.$", RegexOptions.Compiled | RegexOptions.Singleline);
-            public static readonly Regex PartyMemberJoinedAlt = new Regex(LineStartComp + @"LogParty: Verbose: \[(?<DisplayName>.{1,16})\] Id \[MCP:[0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5}\] added to team \[\w+\] at index \[(?<PartyIndex>\d+)\] in \[.{1,16}\]'s game.$", RegexOptions.Compiled | RegexOptions.Singleline);
-            public static readonly Regex PartyMemberLeft = new Regex(LineStartComp + @"LogParty: Display: Party member state for \[(?<DisplayName>.{1,16})\] Id \[MCP:[0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5}\] removed from \[.{1,16}\]'s party\.$", RegexOptions.Compiled | RegexOptions.Singleline);
-            public static readonly Regex StartedGame = new Regex(LineStartComp + @"LogDemo: UReplaySubsystem::RecordReplay: Starting recording with demo driver\.  Name:  FriendlyName: Unsaved Replay$", RegexOptions.Compiled | RegexOptions.Singleline);
-            public static readonly Regex StartedGameAlt = new Regex(LineStartComp + @"LogLocalFileReplay: Writing replay to '.*' with \d+\.\d{2}MB free$", RegexOptions.Compiled | RegexOptions.Singleline);
-            public static readonly Regex LeftGame = new Regex(LineStartComp + @"LogDemo: StopDemo: Demo  stopped at frame \d+$", RegexOptions.Compiled | RegexOptions.Singleline);
+            private static readonly string Timestamp = @"\[\d{4}\.\d{2}\.\d{2}\-\d{2}\.\d{2}\.\d{2}:\d{3}]";
+            private static readonly string UnknownId = @"\[[\d ]{3}\]";
+            private static readonly string LineStart = "^" + Timestamp + UnknownId;
+            public static readonly Regex LoggedIn               = new Regex(LineStart + @"LogOnlineAccount: Display: \[UOnlineAccountCommon::ProcessUserLogin\] Successfully logged in user\. UserId=\[[0-9a-fA-F]{32}\] DisplayName=\[(?<DisplayName>.{1,16})\] EpicAccountId=\[MCP:[0-9a-fA-F]{32}\] AuthTicket=\[<Redacted>\]$", RegexOptions.Compiled | RegexOptions.Singleline);
+            public static readonly Regex PartyMemberJoined      = new Regex(LineStart + @"LogParty: Display: New party member state for \[(?<DisplayName>.{1,16})\] Id \[MCP:[0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5}\] added to the local player's party \[V2:[0-9a-fA-F]{32}\]\.$", RegexOptions.Compiled | RegexOptions.Singleline);
+            public static readonly Regex PartyMemberJoinedAlt   = new Regex(LineStart + @"LogParty: Verbose: \[(?<DisplayName>.{1,16})\] Id \[MCP:[0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5}\] added to team \[\w+\] at index \[(?<PartyIndex>\d+)\] in \[.{1,16}\]'s game.$", RegexOptions.Compiled | RegexOptions.Singleline);
+            public static readonly Regex PartyMemberUpdated     = new Regex(LineStart + @"LogParty: Verbose: \[(?<DisplayName>.{1,16})\] Id \[MCP:[0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5}\] team member data updated, team \[\w+\] at index \[(?<PartyIndex>\d+)\] in \[.{1,16}\]'s game\.", RegexOptions.Compiled | RegexOptions.Singleline);
+            public static readonly Regex PartyMemberLeft        = new Regex(LineStart + @"LogParty: Display: Party member state for \[(?<DisplayName>.{1,16})\] Id \[MCP:[0-9a-fA-F]{5}[0-9a-fA-F\.]{3,22}[0-9a-fA-F]{5}\] removed from \[.{1,16}\]'s party\.$", RegexOptions.Compiled | RegexOptions.Singleline);
+            public static readonly Regex StartedGame            = new Regex(LineStart + @"LogDemo: UReplaySubsystem::RecordReplay: Starting recording with demo driver\.  Name:  FriendlyName: Unsaved Replay$", RegexOptions.Compiled | RegexOptions.Singleline);
+            public static readonly Regex StartedGameAlt         = new Regex(LineStart + @"LogLocalFileReplay: Writing replay to '.*' with \d+\.\d{2}MB free$", RegexOptions.Compiled | RegexOptions.Singleline);
+            public static readonly Regex LeftGame               = new Regex(LineStart + @"LogDemo: StopDemo: Demo  stopped at frame \d+$", RegexOptions.Compiled | RegexOptions.Singleline);
         }
     }
 }
